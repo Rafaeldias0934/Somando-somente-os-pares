bc59610f2eac582a7593a5f21317418488596d16
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-05-12 22:09:40 -0300

    challenge completed

--- src/SomaDosPares.java
+++ src/SomaDosPares.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class SomaDosPares {
     public static void main(String[] args) {
-        int numero;
+        int numeroDigitado = 0, soma = 0;
+        Scanner scan = new Scanner(System.in);
+
+        while (numeroDigitado >= 0) {
+            System.out.println("Informe um número Positivo");
+            numeroDigitado = scan.nextInt();
+            if (numeroDigitado % 2 == 0) {
+                soma += numeroDigitado;
+                System.out.println("A soma dos números positivos digitados é " + soma);
+            } else  {
+                System.out.println("[Programa Encerrado], o número digitado é Impar");
+                break;
+            }
+        }
     }
 }
